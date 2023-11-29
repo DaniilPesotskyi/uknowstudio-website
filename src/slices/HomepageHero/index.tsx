125185a661f8a87b97ee01191d28a82160fcd29e
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-11-29 23:08:49 +0200

    feat: Homepage HeroSlice

--- src/slices/HomepageHero/index.tsx
+++ src/slices/HomepageHero/index.tsx
@@ -1,24 +1,53 @@
+import css from "./index.module.css";
+
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import clsx from "clsx";
 
-/**
- * Props for `HomepageHero`.
- */
 export type HomepageHeroProps = SliceComponentProps<Content.HomepageHeroSlice>;
 
-/**
- * Component for "HomepageHero" Slices.
- */
 const HomepageHero = ({ slice }: HomepageHeroProps): JSX.Element => {
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={css.section}
     >
-      Placeholder component for homepage_hero (variation: {slice.variation})
-      Slices
+      <p className={clsx(css.decoLabel, css.firstLabel)}>01</p>
+      <div className={css.heading}>
+        <p className={clsx(css.title)}>ЗАКАРБУЙ</p>
+        <p className={clsx(css.title)}>ІСТОРІЮ</p>
+        <div className={css.fieldWithSec}>
+          <p className={clsx(css.title)}>В</p>
+          <p className={css.secondaryTitle}>
+            Створюємо акцент на важливих деталях, цікавих моментах та сильних
+            сторонах
+          </p>
+        </div>
+        <p className={clsx(css.title)}>КАДРІ</p>
+      </div>
+      <div className={css.scrollWrap}>
+        <span className={clsx(css.decoLabel)}>SCROLL</span>
+        <button type="button" className={css.scrollBtn}>
+          <ScrollIcon className={css.scrollIcon} />
+        </button>
+        <span className={clsx(css.decoLabel)}>SCROLL</span>
+      </div>
     </section>
   );
 };
 
 export default HomepageHero;
+
+function ScrollIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+    >
+      <path strokeLinecap="square" strokeWidth="2" d="M28 10L15 24 2 10"></path>
+    </svg>
+  );
+}
