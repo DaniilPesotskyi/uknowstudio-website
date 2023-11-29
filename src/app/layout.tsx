5d9564007ab53994903d1be5d55f0609247bf370
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-11-29 17:56:47 +0200

    project init, prismic inti, fonts & metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { createClient } from "@/prismicio";
+
+const nunito = Nunito({
+  subsets: ["cyrillic"],
+  weight: ["400", "500", "600", "700"],
+  style: "normal",
+});
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+
+  const settings = await client.getSingle("settings");
+
+  return {
+    title: settings.data.site_title || "UKNOW? studio",
+    description: settings.data.meta_description || "Photography team website",
+    openGraph: {
+      images: [settings.data.og_image.url || ""],
+    },
+  };
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="uk">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
